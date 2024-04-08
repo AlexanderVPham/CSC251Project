4bfdaad178833e29b225c2ce44fcd52931600597
AlexanderVPham <alex.pham0748@gmail.com>
2024-04-08 09:49:27 -0400

    Update Policy.java
    Added toString method

--- Policy.java
+++ Policy.java
@@ -3,40 +3,28 @@ public class Policy
    //initialize fields
    private int policyNumber;
    private String providerName;
-   private String policyHolderFirstName;
-   private String policyHolderLastName;
-   private int age;
-   private String smokerStatus;
-   private double policyHolderHeight;
-   private double policyHolderWeight;
-   private double BMI = 0;
-   
-   //no-arg constructor
-   public Policy() 
-   {
-      policyNumber = 1111;
-      String providerName = "John Doe";
-      String policyHolderFirstName = "John";
-      String policyHolderLastName = "Doe";
-      age = 18;
-      String smokerStatus = "non-smoker";
-      policyHolderHeight = 62.0;
-      policyHolderWeight = 250.5;
-   }
-   //constructor
-   public Policy(int pN, String providerN, String policyholderFN, String policyholderLN, int a, String smokerS, double policyholderH, double policyholderW)
+
+   //setters and getters
+   public void setPolicyNumber(int pN)
    {
       policyNumber = pN;
-      providerName = providerN;
-      policyHolderFirstName = policyholderFN;
-      policyHolderLastName = policyholderLN;
-      age = a;
-      smokerStatus = smokerS;
-      policyHolderHeight = policyholderH;
-      policyHolderWeight = policyholderW;
    }
+   public int getPolicyNumber()
+   {
+      return policyNumber;
+   }
+   public void setProviderName(String providerN)
+   {
+      providerName = providerN;
+   }
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   
    //toString Method
    public String toString() {
-   
+      return "Policy Number: " + policyNumber + "\n" +
+             "Provider Name: " + providerName;
    }
 }
